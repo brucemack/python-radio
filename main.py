35e6d9faab7aea520d69ede2a9582a80e64d82d8
Bruce MacKinnon <bruce@mackinnon.com>
2020-11-04 07:49:41 -0500

    Now matching EMRFD result

--- main.py
+++ main.py
@@ -105,76 +105,77 @@ Ie_ma = vars[4] * 1000.0
 # Variables
 # 0: Iin - Input from source
 # 1: I1 - From Vb to ground
-# 2: I2 - Into Vb
+# 2: Ib - Into Vb
 # 3: I3 - From Vc to Vb via Rf
 # 4: I4 - Current into the load
-# 5: I5 - Current out emitter towards ground
-# 6: Vc
-# 7: Vb
-# 8: Ve
+# 5: Vc
+# 6: Vb
+# 7: Ve
 
 # Equations
 #
-# 0: Vin = Rs * Iin + Vb
+# 0: Vin = Rs * Iin + R1 * I1
 # 1: Vb = R1 * I1
 #    0 = -Vb + R1 * I1
-# 2: Vb = re * I5 + Ve
-#    0 = -Vb + re * I5 + Ve
+# 2: Vb = re * Ib + b * re * Ib + Ve
+#    0  = -Vb + (re + b * re) * Ib + Ve
 # 3: Vc = Rf * I3 + Vb
 #    0 = -Vc + Rf * I3 + Vb
-# 4: -I4 = I3 + gm * (Vb - Ve)
-#    0 = I4 + I3 + gm * Vb - gm * Ve
+# 4: Ve = Re * Ib + (Re * b) * Ib
+#    0 = -Ve + (Re + Re * b) * Ib
 # 5: Vc = Rl * I4
 #    0 = -Vc + Rl * I4
-# 6: Ve = Re * I5
-#    0 = -Ve + Re * I5
-# 7: I2 = Iin - I1
-#    0 = -I2 + Iin - I1
-# 8: I5 = I2 + I3 + gm * (Vb - Ve)
-#    0 = -I5 + I2 + I3 + gm * Vb - gm * Ve
+# 6: -I4 = I3 + b * Ib
+#    0 = I4 + I3 + b * Ib
+# 7: Iin + I3 = Ib + I1
+#    0 = -Iin -I3 + Ib + I1
 
 print("Bias current in mA", Ie_ma)
 
 # NOTE: There is a bypassed emitter resistor
 Re = parallel_r(47, 8.2)
 Vin = 1
-gm = Ie_ma / 26
 Rs = 50
 # Transformer
 Rl = 200
+b = 300 / 7
 re = 26 / Ie_ma
-print("re = ", re)
+print("re = ", re, " Re = ", Re)
 
 Mss = np.array([
-# Iin, I1,  I2,  I3,  I4,  I5,  Vc,  Vb,  Ve
-[ Rs,  0,   0,   0,   0,   0,   0,   1,   0   ],
-[ 0,   R1,  0,   0,   0,   0,   0,   -1,  0   ],
-[ 0,   0,   0,   0,   0,   re,  0,   -1,  1   ],
-[ 0,   0,   0,   Rf,  0,   0,   -1,  1,   0   ],
-[ 0,   0,   0,   1,   1,   0,   0,   gm,  -gm ],
-[ 0,   0,   0,   0,   Rl,  0,   -1,  0,   0   ],
-[ 0,   0,   0,   0,   0,   Re,  0,   0,   -1  ],
-[ 1,   -1,  -1,  0,   0,   0,   0,   0,   0   ],
-[ 0,   0,   1,   1,   0,   -1,  0,   gm,  -gm ]
+# Iin, I1,  Ib,  I3,  I4,  Vc,  Vb,  Ve
+
+[ Rs,  R1,  0,   0,   0,   0,   0,   0 ],
+[ 0,   R1,  0,   0,   0,   0,   -1,  0 ],
+[ 0,   0, re+re*b, 0, 0,   0,   -1,  1 ],
+[ 0,   0,   0,   Rf,  0,   -1,  1,   0 ],
+[ 0,   0, Re+Re*b, 0, 0,   0,   0,   -1 ],
+[ 0,   0,   0,   0,   Rl,  -1,  0,   0  ],
+[ 0,   0,   b,   1,   1,   0,   0,   0  ],
+[ -1,  1,   1,   -1,  0,   0,   0,   0  ]
 ])
 
-yss = np.array([ Vin, 0, 0, 0, 0, 0, 0, 0, 0])
+yss = np.array([ Vin, 0, 0, 0, 0, 0, 0, 0 ])
 
 Mss_inv = np.linalg.inv(Mss)
-#print(Minv)
 
 vars = Mss_inv.dot(yss)
 print("Small Signal Analysis")
 print("Iin = ", vars[0])
 print("I1 = ", vars[1])
-print("I2 = ", vars[2])
+print("Ib = ", vars[2])
 print("I3 = ", vars[3])
 print("I4 = ", vars[4])
-print("I5 = ", vars[5])
-print("Vc = ", vars[6])
-print("Vb = ", vars[7])
-print("Ve = ", vars[8])
+print("Vc = ", vars[5])
+print("Vb = ", vars[6])
+print("Ve = ", vars[7])
 
-print("Gain = ", -vars[6] / vars[7])
-print("Gain dB = ", 20.0 * math.log10(-vars[6] / vars[7]))
-print("Zin = ", vars[7] / vars[0])
+# Available power (assuming load is matched with source resistance)
+Pav = ((Vin / (Rs + Rs)) ** 2) * Rs
+# Power delivered to load
+Pout = ((vars[5] / Rl) ** 2) * Rl
+# Gain in dB
+G = 10.0 * math.log10(Pout / Pav)
+
+print("Gain dB = ", G)
+print("Zin = ", vars[6] / vars[0])
