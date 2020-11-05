c7fa0b50a4955c35925c007bf988d2d53e556b8a
Bruce MacKinnon <bruce@mackinnon.com>
2020-11-04 20:28:47 -0500

    Refactoring

--- UnitTests1.py
+++ UnitTests1.py
@@ -4,9 +4,13 @@ from BJTFeedback import *
 
 class UnitTests1(unittest.TestCase):
 
-    def test_1(self):
+    def test1(self):
         result = calc_bias(12, 22, 1500, 680, 47, 100)
         self.assertAlmostEqual(result["Ie"], 0.051, places=3)
+
+    def testEMRFD_2_25(self):
+        ssr = calc_small_signal(1, 50, 1300, 1000, 6, 200, 0.020, 21)
+        self.assertAlmostEqual(ssr["gain"], 20, places=0)
 
 
 if __name__ == '__main__':
